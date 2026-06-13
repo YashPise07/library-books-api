de05c848d4777815c9697eb4b964af298b1fa3c2
Shreyash Pise <yashpise0803@gmail.com>
2026-06-13 12:16:57 +0530

    use neon database

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Yash@localhost/librarydb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
